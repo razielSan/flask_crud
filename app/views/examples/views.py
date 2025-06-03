d26db3a0c24ad6a74b8919e02279ea800013b27a
raziel <tvindzon@gmail.com>
2025-06-03 20:18:40 +0700

    add example hover and click + hold ctrl

--- app/views/examples/views.py
+++ app/views/examples/views.py
@@ -9,13 +9,15 @@ def examples_list():
     return render_template("examples/examples.html")
 
 
-@router.route(
-    "/ping",
-    endpoint="ping",
-    methods=["POST", "GEt"]
-)
+@router.route("/ping", endpoint="ping", methods=["POST", "GEt"])
 def handle_ping():
     if request.method == "POST":
         return "Pong!"
     return render_template("examples/ping/show_ping.html")
 
+
+@router.route("/hover", methods=["POST", "GET"], endpoint="hover")
+def handle_hover():
+    if request.method == "POST":
+        return "I see you!"
+    return render_template("examples/hover/show_hover.html")
