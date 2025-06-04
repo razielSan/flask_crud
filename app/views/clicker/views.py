27d9c22b1cfca9232117614388f0ac27f135df62
raziel <tvindzon@gmail.com>
2025-06-04 20:13:44 +0700

    Embed clicker into index page

--- app/views/clicker/views.py
+++ app/views/clicker/views.py
@@ -12,22 +12,23 @@ clicker = Clicker()
 @router.route("/", methods=["POST", "GET"], endpoint="click")
 def show_clicker_page():
     if request.method == "POST":
+        template_name = "clicker/clicker.html"
         if is_backround_request():
             clicker.inc_count()
-            return render_template(
-                "clicker/components/_clicker-count.html",
-                count=clicker.count,
-                form_count=clicker.count_form,
-            )
+            template_name = "clicker/components/_clicker-count.html"
         else:
             clicker.inc_count_form()
-            return render_template(
-                "clicker/clicker.html",
-                count=clicker.count,
-                form_count=clicker.count_form,
-            )
+        return render_template(
+            template_name,
+            count=clicker.count,
+            form_count=clicker.count_form,
+        )
+
+    template_name = "clicker/clicker.html"
+    if is_backround_request():
+        template_name = "clicker/components/_clicker-body.html"
     return render_template(
-        "clicker/clicker.html",
+        template_name,
         count=clicker.count,
         form_count=clicker.count_form,
     )
