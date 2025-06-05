98313213b91c3e9e92d058c10a875fe20ca9c9c6
raziel <tvindzon@gmail.com>
2025-06-05 15:47:55 +0700

    create product create on list

--- app/views/products/views.py
+++ app/views/products/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, render_template
+from flask import Blueprint, request, render_template, redirect, url_for
 
 from views.products.crud import product_storage
 
@@ -8,8 +8,15 @@ router = Blueprint("products", __name__)
 @router.get("/")
 def get_product_list():
 
-
     return render_template(
         "products/list.html",
         list_products=product_storage.get_list,
     )
+
+
+@router.post("/")
+def create_product():
+    price = request.form.get("price")
+    name = request.form.get("name")
+    product_storage.add(name=name, price=price)
+    return redirect(url_for("products.get_product_list"))
