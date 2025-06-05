ac80f921f248cc2b4b51fe456986692c17d3dbef
raziel <tvindzon@gmail.com>
2025-06-05 13:03:58 +0700

    handle boosts

--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -2,4 +2,7 @@ from flask import request
 
 
 def is_backround_request():
-    return bool(request.headers.get("Hx-Request"))
+    hx_request = request.headers.get("Hx-Request")
+    boosted = request.headers.get("Hx-Boosted")
+    return bool(hx_request) and not boosted
+
